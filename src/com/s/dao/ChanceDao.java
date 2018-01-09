8e91e8f5ba7da5f9440abfc088996e3daa9ce4ff
ordinaryzhen <1723732113@qq.com>
2018-01-09 09:46:48 +0800

    销售机会

---
+++ src/com/s/dao/ChanceDao.java
@@ -0,0 +1,13 @@
+package com.s.dao;
+
+import java.util.List;
+
+import com.s.entity.SalChance;
+
+public interface ChanceDao {
+	//ĞÂÔö
+	public int save(SalChance chance);
+
+	public List query();
+
+}
---
+++ src/com/s/dao/impl/ChaceDaoImpl.java
@@ -0,0 +1,24 @@
+package com.s.dao.impl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import com.s.dao.ChanceDao;
+import com.s.entity.SalChance;
+
+public class ChaceDaoImpl extends HibernateDaoSupport implements ChanceDao{
+	//ÐÂÔö
+	public int save(SalChance chance) {
+		Session session = this.getSession();
+		return (Integer)session.save(chance);
+	}
+//²éÑ¯
+	public List query() {
+		Session session = this.getSession();
+		List<SalChance> list= (List) session.createCriteria(SalChance.class).list();
+		return list;
+	}
+
+}
